213a9f827bdd0fc6a85e99b7a25d10877b852c87
Hajar Zaitouni <hajarzaitouni88@gmail.com>
2024-05-29 16:05:03 +0100

    task 6

---
+++ 0x01-ES6_promise/6-final-user.js
@@ -0,0 +1,12 @@
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
+
+export default function handleProfileSignup(firstName, lastName, fileName) {
+  const user = signUpUser(firstName, lastName);
+  const photo = uploadPhoto(fileName);
+
+  return Promise.allSettled([user, photo]).then((result) => result.map((res) => ({
+    status: res.status,
+    value: res.value || res.reason,
+  })));
+}
