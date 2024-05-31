40f598b55114f8eec98a66ac3a5edf25b31830ac
Hajar Zaitouni <hajarzaitouni88@gmail.com>
2024-05-31 23:18:29 +0100

    add task 10

---
+++ 0x00-ES6_basic/10-loops.js
@@ -0,0 +1,8 @@
+export default function appendToEachArrayValue(array, appendString) {
+  const newarr = [];
+  for (const value of array) {
+    newarr.push(`${appendString}${value}`);
+  }
+
+  return newarr;
+}
