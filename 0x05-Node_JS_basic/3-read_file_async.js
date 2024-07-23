645694e0d91f405473ab91bf5970394cb3c8ee44
Hajar Zaitouni <hajarzaitouni88@gmail.com>
2024-07-24 00:10:14 +0100

    task 3, read from a file using promise

---
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -0,0 +1,33 @@
+const fs = require('fs');
+
+function countStudents(path) {
+  return new Promise((resolve, reject) => {
+    fs.readFile(path, { encoding: 'utf8' }, (err, data) => {
+      if (err) {
+        reject(new Error('Cannot load the database'));
+        return;
+      }
+      let lines = data.toString().split('\n');
+
+      lines = lines.slice(1, lines.length - 1);
+      console.log(`Number of students: ${lines.length}`);
+
+      const fields = {};
+      for (const line of lines) {
+        const student = line.split(',');
+        if (!fields[student[3]]) {
+          fields[student[3]] = [];
+        }
+        fields[student[3]].push(student[0]);
+      }
+      for (const field in fields) {
+        if (field) {
+          const list = fields[field];
+          console.log(`Number of students in ${field}: ${list.length}. List: ${list.join(', ')}`);
+        }
+      }
+      resolve();
+    });
+  });
+}
+module.exports = countStudents;
