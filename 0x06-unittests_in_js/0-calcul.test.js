90ba4c3a513e4af8291e566738207d87d5d111a6
Hajar Zaitouni <hajarzaitouni88@gmail.com>
2024-07-25 19:45:46 +0100

    task 0

---
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -0,0 +1,20 @@
+const calculateNumber = require('./0-calcul.js');
+const assert = require('assert');
+
+describe('calculateNumber', () => {
+  it('should return 4', () => {
+    assert.strictEqual(calculateNumber(1, 3), 4);
+  });
+
+  it('should return 5', () => {
+    assert.strictEqual(calculateNumber(1, 3.7), 5);
+  });
+
+  it('should return 5', () => {
+    assert.strictEqual(calculateNumber(1.2, 3.7), 5);
+  });
+
+  it('should return 6', () => {
+    assert.strictEqual(calculateNumber(1.5, 3.7), 6);
+  });
+});
