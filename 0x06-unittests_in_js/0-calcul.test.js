2a488c1cf85e136e94084502cba8ec71df6ccbce
Hajar Zaitouni <hajarzaitouni88@gmail.com>
2024-07-25 20:00:56 +0100

    add tests

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -6,8 +6,16 @@ describe('calculateNumber', () => {
     assert.strictEqual(calculateNumber(1, 3), 4);
   });
 
+  it('should return 4', () => {
+    assert.strictEqual(calculateNumber(1.2, 3), 4);
+  });
+
   it('should return 5', () => {
     assert.strictEqual(calculateNumber(1, 3.7), 5);
+  });
+
+  it('should return 6', () => {
+    assert.strictEqual(calculateNumber(3, 3.1),6);
   });
 
   it('should return 5', () => {
@@ -16,5 +24,9 @@ describe('calculateNumber', () => {
 
   it('should return 6', () => {
     assert.strictEqual(calculateNumber(1.5, 3.7), 6);
+  });
+
+  it('should return 1', () => {
+    assert.strictEqual(calculateNumber(0.7, 0.4), 1);
   });
 });
