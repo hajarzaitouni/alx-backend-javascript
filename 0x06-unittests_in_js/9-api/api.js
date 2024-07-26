6cb9aa677971accf1208502e2af44be9d11c4510
Hajar Zaitouni <hajarzaitouni88@gmail.com>
2024-07-26 02:04:01 +0100

    fix

--- 0x06-unittests_in_js/9-api/api.js
+++ 0x06-unittests_in_js/9-api/api.js
@@ -7,8 +7,8 @@ app.get('/', (req, res) => {
   res.send('Welcome to the payment system');
 });
 
-app.get('/cart/:id', (req, res) => {
-  res.end(`Payment methods for cart ${req.params.id}`);
+app.get('/cart/:id(\\d+)', (req, res) => {
+  res.send(`Payment methods for cart ${req.params.id}`);
 });
 
 app.listen(port, () => {
